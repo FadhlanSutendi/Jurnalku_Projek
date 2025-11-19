15f9b497979db2b09ba9da92238c959c8410c17e
Julianvin <delvinaping10@gmail.com>
2025-11-19 14:54:04 +0700

    add route

--- lib/widgets/dashboard_app_bar.dart
+++ lib/widgets/dashboard_app_bar.dart
@@ -3,6 +3,7 @@ import 'package:jurnalku_projek/dashboard_page.dart';
 import 'package:jurnalku_projek/explore_page.dart';
 import 'package:jurnalku_projek/profile_page.dart';
 import 'package:jurnalku_projek/panduan_pengguna_page.dart';
+import 'package:jurnalku_projek/account_settings.dart';
 
 class DashboardAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String titleText;
@@ -172,7 +173,10 @@ class DashboardAppBar extends StatelessWidget implements PreferredSizeWidget {
               child: InkWell(
                 onTap: () {
                   Navigator.pop(context);
-                  Navigator.pushNamed(context, '');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AccountSettingsPage()),
+                  );
                 },
                 child: const Row(
                   children: [
